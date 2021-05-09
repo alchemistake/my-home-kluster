0afd247c2a627b2629a05e429beaf12b3f1759a1
Alchemistake <alchemistake@gmail.com>
2021-05-09 21:39:33 +0200

    Zerotier requires sudo

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ curl -sfL https://get.k3s.io | sh -
 # Install Zerotier
 curl -s https://install.zerotier.com | sudo bash
 
-zerotier-cli join abfd31bd47704311 # The Generalized Network
-zerotier-cli join abfd31bd47879a95 # The Quake Network
+sudo zerotier-cli join abfd31bd47704311 # The Generalized Network
+sudo zerotier-cli join abfd31bd47879a95 # The Quake Networknan
