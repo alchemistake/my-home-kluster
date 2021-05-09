5f543715676c1de456aee672454be639eed9f9dd
Alchemistake <alchemistake@gmail.com>
2021-05-09 21:04:59 +0200

    Workaround Install APT transport https

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Install Dependencies
-sudo apt install -y curl, git
+sudo apt-get install -y curl git apt-transport-https
 
 # Install K3S
 curl -sfL https://get.k3s.io | sh -
 
 # Install Zerotier
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
-# curl -s https://install.zerotier.com | sudo bash
+curl -s https://install.zerotier.com | sudo bash
 
 zerotier-cli join abfd31bd47704311 # The Generalized Network
 zerotier-cli join abfd31bd47879a95 # The Quake Network
