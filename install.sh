2eca431b990cda0393d49905b057b2a7ac579855
Alchemistake <alchemistake@gmail.com>
2021-05-09 20:46:32 +0200

    Install and join Zerotier networks

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ sudo apt install -y curl, git
 
 # Install K3S
 curl -sfL https://get.k3s.io | sh -
+
+# Install Zerotier
+curl -s https://install.zerotier.com | sudo bash
+
+zerotier-cli join abfd31bd47704311 # The Generalized Network
+zerotier-cli join abfd31bd47879a95 # The Quake Network
