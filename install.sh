19e7e926ef769ec37d2a1374038d14a15d88a303
Alchemistake <alchemistake@gmail.com>
2021-05-09 20:52:45 +0200

    GPG Zerotier

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ sudo apt install -y curl, git
 curl -sfL https://get.k3s.io | sh -
 
 # Install Zerotier
-curl -s https://install.zerotier.com | sudo bash
+curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
+if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+# curl -s https://install.zerotier.com | sudo bash
 
 zerotier-cli join abfd31bd47704311 # The Generalized Network
 zerotier-cli join abfd31bd47879a95 # The Quake Network
