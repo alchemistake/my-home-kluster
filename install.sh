19333dcb49ad9001bf0d4773730bb87cd19bda84
Alchemistake <alchemistake@gmail.com>
2021-05-29 22:27:19 +0200

    removed stupid commas

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install Dependencies
 sudo apt update -y
-sudo apt install -y curl, git, ntfs-3g, hfsprogs
+sudo apt install -y curl git ntfs-3g hfsprogs
 
 # Install K3S
 curl -sfL https://get.k3s.io | sh -
