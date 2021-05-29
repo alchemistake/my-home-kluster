0e05dd3beba60f1cedaddb29b8f4dae4120481be
Caner Çalışkaner <caner.caliskaner@cognite.com>
2021-05-29 22:38:36 +0200

    File Systems are added (#3)
    * Added ntfs-3g
    
    * added HFS support
    
    * removed stupid commas
    
    * Added exit on error
    
    * Fixed Upgrade
    
    * File Permissions Fixed
    
    * Trailing New Line

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
 # Install Dependencies
-sudo apt install -y curl, git
+sudo apt upgrade -y
+sudo apt install -y curl git ntfs-3g hfsprogs
 
 # Install K3S
 curl -sfL https://get.k3s.io | sh -
