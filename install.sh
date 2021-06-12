90a008fb985f7e3024e28625cc4f827a06494f44
Caner Caliskaner <caner.caliskaner@cognite.com>
2021-06-12 17:09:58 +0200

    Fix sudo, added ssh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+# TODO remove sudos redundant
 set -e
 
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
 # Install Dependencies
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y network-manager curl git ntfs-3g hfsprogs mbpfan linux-headers-generic build-essential dkms bcmwl-kernel-source
+apt update -y
+apt upgrade -y
+apt install -y network-manager curl git ntfs-3g hfsprogs mbpfan linux-headers-generic build-essential dkms bcmwl-kernel-source openssh-server
 
 # mbpfan Config
-sudo echo "[general]
+echo "[general]
 
 min_fan_speed = $(cat '/sys/devices/platform/applesmc.768/fan1_min')
 max_fan_speed = $(cat '/sys/devices/platform/applesmc.768/fan1_max')
@@ -16,11 +22,27 @@ high_temp = 66
 max_temp = 85
 polling_interval = 1
 " > /etc/mbpfan.conf
-sudo service mbpfan restart
+service mbpfan restart
+
+curl -sfL https://raw.githubusercontent.com/linux-on-mac/mbpfan/master/mbpfan.service > /etc/systemd/system/mbpfan.service
+systemctl enable mbpfan.service
+systemctl daemon-reload
+systemctl start mbpfan.service
+
+# SSH Config
+echo "Include /etc/ssh/ssh_config.d/*.conf
+
+Host *
+PasswordAuthentication yes
+SendEnv LANG LC_*
+HashKnownHosts yes
+" > /etc/ssh/ssh_config
+systemctl enable sshd.service
+systemctl start sshd.service
 
 # Disable Lid Sleep
 echo " HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
-sudo service systemd-logind restart
+service systemd-logind restart
 
 # Install the Wifi Driver
 git clone https://github.com/clnhub/rtl8192eu-linux.git
