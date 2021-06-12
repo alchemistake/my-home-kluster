da2b905f306998374b4806604141883475777c3b
Caner Çalışkaner <alchemistake@gmail.com>
2021-06-12 09:43:33 +0200

    Wifi (#5)
    * Wifi Driver
    
    * Fixed File Perm, Added Network Manager
    
    * Windows File Returns are annoying
    
    * Fan id fix
    
    * File should not append
    
    * Macbook Wifi Drivers

--- install.sh
+++ install.sh
@@ -2,20 +2,27 @@
 set -e
 
 # Install Dependencies
+sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y curl git ntfs-3g hfsprogs mbpfan
+sudo apt install -y network-manager curl git ntfs-3g hfsprogs mbpfan linux-headers-generic build-essential dkms bcmwl-kernel-source
 
 # mbpfan Config
 sudo echo "[general]
 
-min_fan_speed = $(cat '/sys/devices/platform/applesmc.768/fan*_min')
-max_fan_speed = $(cat '/sys/devices/platform/applesmc.768/fan*_max')
+min_fan_speed = $(cat '/sys/devices/platform/applesmc.768/fan1_min')
+max_fan_speed = $(cat '/sys/devices/platform/applesmc.768/fan1_max')
 low_temp = 63
 high_temp = 66
 max_temp = 85
 polling_interval = 1
-" >> /etc/mbpfan.conf
+" > /etc/mbpfan.conf
 sudo service mbpfan restart
+
+# Install the Wifi Driver
+git clone https://github.com/clnhub/rtl8192eu-linux.git
+cd rtl8192eu-linux
+./install_wifi.sh
+cd ..
 
 # Install K3S
 curl -sfL https://get.k3s.io | sh -
